e7acd111c33080be279ca1153acd6fd26094c9bb
kimcanchola <kimcannchola1@gmail.com>
2023-09-20 14:13:22 -0700

    Added navbar and base homepage

--- rnr/src/App.js
+++ rnr/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav'
+import NavBar from './components/NavBar'
+import HomePage from './pages/HomePage';
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      
+      <NavBar/>
+      <HomePage/>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     </div>
   );
 }
---
+++ rnr/src/pages/HomePage.jsx
@@ -0,0 +1,84 @@
+
+const HomePage = () => {
+    return(
+        <div id='HomePage' className="page-simple">
+            <div class="flex basis-9/12 mx-auto h-full p-4 bg-slate-50">
+<div class="grid grid-cols-2 md:grid-cols-2 gap-4 items-center max-w-fit">
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0=" alt=""/>
+        </div>
+        <div>
+        <div id="detailed-pricing" class="w-full overflow-x-auto">
+    <div class="overflow-hidden min-w-max">
+        <div class="grid grid-cols-1 p-4 text-lg font-medium text-gray-900 bg-gray-100 border-t border-b border-gray-200 gap-x-16 dark:bg-gray-800 dark:border-gray-700 dark:text-white">
+            <div class="flex items-center">Hours</div>
+           
+        </div>
+        <div class="grid grid-cols-2 px-4 py-5 text-md text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+            
+        <div class="text-gray-500 dark:text-gray-400">Monday - Thursday</div>
+            <div>
+            <div class="text-gray-500 dark:text-gray-400">11:30 PM - 2:00 PM</div>
+            <div class="text-gray-500 dark:text-gray-400">4:30 PM - 9:00 PM</div>
+            </div>
+        </div>
+        <div class="grid grid-cols-2 px-4 py-5 text-md text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+        <div>
+            <div class="text-gray-500 dark:text-gray-400">Friday</div>
+            </div>
+            <div>
+            <div class="text-gray-500 dark:text-gray-400">11:30 PM - 9:00 PM</div>
+            </div>
+            
+            
+        </div>
+        <div class="grid grid-cols-2 px-4 py-5 text-md text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+            
+            <div>
+            <div class="text-gray-500 dark:text-gray-400">Saturday</div>
+            </div>
+            <div>
+            <div class="text-gray-500 dark:text-gray-400">11:30 PM - 9:00 PM</div>
+            </div>
+            
+        </div>
+        <div class="grid grid-cols-2 px-4 py-5 text-md text-gray-700 border-b border-gray-200 gap-x-16 dark:border-gray-700">
+            <div class="text-gray-500 dark:text-gray-400">Sunday</div>
+            <div>
+            <div class="text-gray-500 dark:text-gray-400">12:00 PM - 2:00 PM</div>
+            <div class="text-gray-500 dark:text-gray-400">4:30 PM - 9:00 PM</div>
+            </div>
+            
+        </div>
+       
+    </div>
+</div>
+        </div>
+        <div>
+        <p>filler</p>
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+        <blockquote class="text-5xl italic font-semibold text-gray-900 dark:text-white">
+            <p>Sushi that Rocks Your World, One Roll at a Time.</p>
+        </blockquote>
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg" src="https://media.istockphoto.com/id/1053854126/photo/all-you-can-eat-sushi.jpg?s=612x612&w=0&k=20&c=qqPJBYcxR0fgmzIFj_k2V6Mbo12hBBCucs1i5HcGYA0=" alt=""/>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
+
+            
+       
+    );
+};
+
+export default HomePage;
